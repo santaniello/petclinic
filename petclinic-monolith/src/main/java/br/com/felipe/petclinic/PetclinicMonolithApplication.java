6a7be19f927202be3e5ddc436da695b1a80c94da
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2023-02-09 11:45:48 -0300

    Initial commit

---
+++ petclinic-monolith/src/main/java/br/com/felipe/petclinic/PetclinicMonolithApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicMonolithApplication.class, args);
+	}
+
+}
